6f08cecac8839cfd6c305d991faa27cfa618845f
lonethan <lonethan0167@gmail.com>
2026-04-13 14:38:18 +0200

    intial do while loop

--- CSI142 Mini Project/src/com/ub/csi142/transport/app/Main.java
+++ CSI142 Mini Project/src/com/ub/csi142/transport/app/Main.java
@@ -1,3 +1,8 @@
-public class Main {
 
+import com.ub.csi142.transport.menu.Menu;
+public class Main {
+    public static void main(String[] args){
+        Menu menu = new Menu();
+        menu.displayMenu();
+    }
 }
--- CSI142 Mini Project/src/com/ub/csi142/transport/menu/Menu.java
+++ CSI142 Mini Project/src/com/ub/csi142/transport/menu/Menu.java
@@ -1,5 +1,42 @@
 package com.ub.csi142.transport.menu;
 
+import java.util.Scanner;
 public class Menu {
-    
-}
+    public void displayMenu(){
+        Scanner in = new Scanner(System.in);
+
+        int choice;
+        do{
+            System.out.println("Welcome to the Transpotation System!\n");
+            System.out.println("1) Add new Vehicle");
+            System.out.println("2) Add new driver");
+            System.out.println("3) Add new route");
+            System.out.println("4) Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = in.nextInt();
+
+            switch (choice) { //Switch staments are simple and easy but we could later add options which have methods
+                case 1:
+                    System.out.println("You have chosen to add a new vehicle.\n");
+                    break;
+                case 2:
+                    System.out.println("You have chosen to add a new driver.\n");
+                    break;
+                case 3:
+                    System.out.println("You have chosen to add a new route.\n");
+                    break;
+                case 4:
+                    System.out.println("Exiting. Good day!");
+                    break;
+                default:
+                    System.out.println("Invalid choice, Please try again.\n");
+        
+                }
+            }
+            while (choice != 4);
+            
+        in.close();
+        }
+
+    }
