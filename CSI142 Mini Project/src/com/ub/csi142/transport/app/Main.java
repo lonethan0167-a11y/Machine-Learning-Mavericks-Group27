9c6165d7c37f888bf3a2b642ae8c0eaa11c6dc9f
raexis-lab <rachelmolapo70@gmail.com>
2026-04-14 16:21:05 +0200

    Remove unnecessary blank line in Main.java

--- CSI142 Mini Project/src/com/ub/csi142/transport/app/Main.java
+++ CSI142 Mini Project/src/com/ub/csi142/transport/app/Main.java
@@ -1,7 +1,6 @@
 package com.ub.csi142.transport.app;
 
 import com.ub.csi142.transport.menu.Menu;
-
 public class Main {
     public static void main(String[] args){
         Menu menu = new Menu();
