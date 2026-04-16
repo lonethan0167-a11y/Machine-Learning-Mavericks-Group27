3a838e512adad9375f13c18cdb6f52412456c7b3
raexis-lab <rachelmolapo70@gmail.com>
2026-04-16 13:35:36 +0200

    Make Person class abstract and validate name
    Changed Person class to an abstract class and added validation for name.

--- CSI142 Mini Project/src/com/ub/csi142/transport/model/Person.java
+++ CSI142 Mini Project/src/com/ub/csi142/transport/model/Person.java
@@ -1,11 +1,16 @@
 package com.ub.csi142.transport.model;
 
-public class Person {
+public abstract class Person {
     private String name;
     public Person(String name){
+        if(name == null || name.isEmpty()){
+            throw new
+                IllegalArgumentException("Name cannot be empty");
+        }
         this.name=name;
     }
     public String getName(){
         return name;
     }
+    public abstract String getRole();
 }
