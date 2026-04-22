dbfec89d2d3449fee4680129dc40d98262fcb289
lonethan <lonethan0167@gmail.com>
2026-04-22 16:08:43 +0200

    Booking classes

---
+++ CSI142 Mini Project/src/com/ub/csi142/transport/model/Bookingcombi.java
@@ -0,0 +1,28 @@
+package com.ub.csi142.transport.model;
+
+public class Bookingcombi {
+    private static int counter = 1;
+
+    private String bookingId;
+    private Passenger passenger;
+    private Tripcombi trip;
+    private int seats;
+
+    public Bookingcombi(Passenger passenger, Tripcombi trip, int seats) {
+        this.bookingId = "B" + (counter++);
+        this.passenger = passenger;
+        this.trip = trip;
+        this.seats = seats;
+    }
+
+    public String getBookingId() { return bookingId; }
+    public Passenger getPassenger() { return passenger; }
+    public Tripcombi getTrip() { return trip; }
+    public int getSeats() { return seats; }
+
+    @Override
+    public String toString() {
+        return "Booking " + bookingId + " | " + passenger.getFullName()
+                + " | Trip " + trip.getTripId() + " | Seats: " + seats;
+    }
+}
---
+++ CSI142 Mini Project/src/com/ub/csi142/transport/model/Bookingtaxi.java
@@ -0,0 +1,29 @@
+package com.ub.csi142.transport.model;
+
+public class Bookingtaxi {
+    public static int counter = 1;
+
+    private String bookingId;
+    private Passenger passenger;
+    private Triptaxi trip;
+    private int seats;
+
+    public Bookingtaxi(Passenger passenger, Triptaxi trip, int seats) {
+        this.bookingId = "B" + (counter++);
+        this.passenger = passenger;
+        this.trip = trip;
+        this.seats = seats;
+    } 
+    
+    public String getBookingId() { return bookingId;}
+    public Passenger getPassenger() { return passenger; }   
+    public Triptaxi getTrip() { return trip; }
+    public int getSeats() { return seats; }
+
+    @Override
+    public String toString() {
+        return "Booking " + bookingId + " | " + passenger.getFullName()
+                + " | Trip " + trip.getTripId() + " | Seats: " + seats;
+    }
+
+}
